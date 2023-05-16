1019b830209321c81fde26037587cbae114fa376
Yafann <127930284+Yafann@users.noreply.github.com>
2023-05-17 01:52:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const mysql = require('mysql2');
+
+var jwt = require("jsonwebtoken");
+var secrat = "sadasdasd";
+
 require('dotenv').config()
 
 const connection = mysql.createConnection(process.env.DATABASE_URL)
@@ -19,6 +23,40 @@ app.get("/api", (req, res) => {
   })
 })
 
+app.get("/api/:id", (req, res) => {
+  const id = req.params.id;
+  const q = "SELECT * FROM contact WHERE id = ?"
+  connection.query(q,id,(err,data)=>{
+      if(err) return res.json(err)
+      return res.json(data)
+  })
+})
+
+app.post("/login", (req, res) => {
+  const q = "SELECT * FROM admin WHERE username = ? AND password = ?"
+  connection.query(q,[req.body.username,req.body.password],(err,data)=>{
+      if(err) {
+        return res.json(err)
+      }else{
+        if(data.length > 0){
+          var token = jwt.sign({ username: data[0].username }, secrat, {
+            expiresIn: "1h",
+          });
+          return res.json({
+            status: "success",
+            message: "เข้าสู่ระบบสำเร็จ",
+            token,
+          })
+        }else{
+          return res.json({
+            status: "error",
+            message: "ผิดพลาด",
+          })
+        }
+      }
+  })
+})
+
 app.post("/sendapi", (req, res) => {
   const fname = req.body.firstName;
   const lname = req.body.lastName;
@@ -30,6 +68,33 @@ app.post("/sendapi", (req, res) => {
         return res.json(err)
       }
       return res.json({ status: "success", message: "insert success" })
+  })
+})
+
+
+app.post("/update", (req, res) => {
+  const id = req.body.id;
+  const fname = req.body.firstName;
+  const lname = req.body.lastName;
+  const email = req.body.email;
+  const phone = req.body.phone;
+  const message = req.body.message;
+  connection.query("UPDATE contact SET fname = ?, lname = ?, email = ?, phone = ?, message = ? WHERE id = ?",[fname,lname,email,phone,message,id],(err,data)=>{
+      if(err) {
+        return res.json(err)
+      }
+      return res.json({ status: "success", message: "update success" })
+  })
+})
+
+
+app.delete("/del/:id", (req, res) => {
+  const id = req.params.id;
+  connection.query("DELETE FROM  contact WHERE id = ?",id,(err,data)=>{
+      if(err) {
+        return res.json(err)
+      }
+      return res.json({ status: "success", message: "delete success" })
   })
 })
 
